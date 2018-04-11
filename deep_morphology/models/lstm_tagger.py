933e8c817e065959870c8b7c0370631913b7016c
Judit Acs <judit@sch.bme.hu>
2018-04-11 18:46:08 +0200

    dropout

--- deep_morphology/models/lstm_tagger.py
+++ deep_morphology/models/lstm_tagger.py
@@ -28,10 +28,11 @@ class LSTMTagger(BaseModel):
     def __init__(self, config, input_size, output_size):
         super().__init__(config, input_size, output_size)
         self.hidden_size = self.config.hidden_size_src
+        self.embedding_dropout = nn.Dropout(self.config.dropout)
         self.embedding = nn.Embedding(input_size, self.config.embedding_size_src)
         nn.init.xavier_uniform(self.embedding.weight)
         self.cell = nn.LSTM(self.config.embedding_size_src, self.hidden_size,
-                            batch_first=True,
+                            batch_first=True, dropout=self.config.dropout,
                             num_layers=self.config.num_layers_src, bidirectional=True)
         self.out_proj = nn.Linear(self.hidden_size, output_size)
         self.criterion = nn.CrossEntropyLoss(ignore_index=Vocab.CONSTANTS['PAD'])
@@ -39,6 +40,7 @@ class LSTMTagger(BaseModel):
     def forward(self, input):
         input = to_cuda(Variable(input[0].long()))
         embedded = self.embedding(input)
+        embedded = self.embedding_dropout(embedded)
         output, hidden = self.cell(embedded)
         output = output[:, :, :self.hidden_size] + output[:, :, self.hidden_size:]
         output = self.out_proj(output)
