a7cf945149eba62dc09d3c343eac4c5668ce8e38
Judit Acs <judit@sch.bme.hu>
2018-06-11 12:07:39 +0200

    compute train acc too

--- scripts/sigmorphon/evaluate_dirs.sh
+++ scripts/sigmorphon/evaluate_dirs.sh
@@ -7,23 +7,36 @@
 #
 
 for exp_dir in "$@"; do
+    if [ ! -f $exp_dir/config.yaml ]; then
+        echo "$exp_dir is not an experiment dir"
+        continue
+    fi
+
+    train_file=$(grep ^train_file $exp_dir/config.yaml)
+    train_file=${train_file#train_file: }
+    if [ ! -f $exp_dir/train.word_accuracy ] || [ $exp_dir/model -nt $exp_dir/train.word_accuracy ]; then
+        echo "EXPERIMENT $exp_dir"
+        echo "   Evaluating train file: $train_file"
+        python deep_morphology/inference.py -e $exp_dir -t $train_file > $exp_dir/train.out
+        paste $train_file <( cut -f2 $exp_dir/train.out ) | sed 's/ //g' | awk 'BEGIN{FS="\t"}{if($2==$NF)c++;s++}END{print c/s}' > $exp_dir/train.word_accuracy
+        cat $exp_dir/train.word_accuracy
+    fi
+
     dev_file=$(grep ^dev_file $exp_dir/config.yaml)
     dev_file=${dev_file#dev_file: }
     if [ ! -f $exp_dir/dev.word_accuracy ] || [ $exp_dir/model -nt $exp_dir/dev.word_accuracy ]; then
-        echo $exp_dir
-        echo $dev_file
+        echo "   Evaluating dev file: $dev_file"
         python deep_morphology/inference.py -e $exp_dir -t $dev_file > $exp_dir/dev.out
-        paste <(sed 's/ //g' $dev_file ) <( cut -f2 $exp_dir/dev.out ) | awk 'BEGIN{FS="\t"}{if($2==$4)c++;s++}END{print c/s}' > $exp_dir/dev.word_accuracy
+        paste $dev_file <( cut -f2 $exp_dir/dev.out ) | sed 's/ //g' | awk 'BEGIN{FS="\t"}{if($2==$NF)c++;s++}END{print c/s}' > $exp_dir/dev.word_accuracy
         cat $exp_dir/dev.word_accuracy
     fi
     test=$(dirname $dev_file)
     test=${test}/../../answers
     if [ -f $test ]; then
         if [ ! -f $exp_dir/test.word_accuracy ] || [ $exp_dir/model -nt $exp_dir/test.word_accuracy ]; then
-            echo $exp_dir
-            echo $test
+            echo "   Evaluating test file: $test"
             python deep_morphology/inference.py -e $exp_dir -t $test > $exp_dir/test.out
-            paste <(sed 's/ //g' $test ) <( cut -f2 $exp_dir/test.out ) | awk 'BEGIN{FS="\t"}{if($2==$4)c++;s++}END{print c/s}' > $exp_dir/test.word_accuracy
+            paste $test <( cut -f2 $exp_dir/test.out ) | sed 's/ //g' | awk 'BEGIN{FS="\t"}{if($2==$NF)c++;s++}END{print c/s}' > $exp_dir/test.word_accuracy
             cat $exp_dir/test.word_accuracy
         fi
     fi
