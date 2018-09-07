90bc06ba00deae53c1cf4bab30a4fbeea95e2e7e
Judit Acs <judit@sch.bme.hu>
2018-09-07 21:05:22 +0200

    remove deleted models

--- deep_morphology/models/__init__.py
+++ deep_morphology/models/__init__.py
@@ -1,7 +1,5 @@
 from deep_morphology.models.base import BaseModel
-from deep_morphology.models.dummy import DummyModel
 from deep_morphology.models.hard_monotonic_attention import HardMonotonicAttentionSeq2seq
 from deep_morphology.models.luong_attention import LuongAttentionSeq2seq
 from deep_morphology.models.lstm_tagger import LSTMTagger
 from deep_morphology.models.reinflection_seq2seq import ReinflectionSeq2seq
-#from deep_morphology.models.sigmorphon_task2_model import ContextInflectionSeq2seq, TwoHeadedContextAttention, MorphoSyntaxModel
