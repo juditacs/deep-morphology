c7209e3be6131c8cd96d6536c3831c44e0540d6e
Judit Acs <judit@sch.bme.hu>
2018-10-03 16:56:26 -0700

    pad at the dataset level - needed for CNN

--- deep_morphology/data/__init__.py
+++ deep_morphology/data/__init__.py
@@ -2,6 +2,7 @@
 
 from deep_morphology.data.seq2seq_data import Seq2seqDataset, UnlabeledSeq2seqDataset
 from deep_morphology.data.seq2seq_data import InflectionDataset, UnlabeledInflectionDataset
+from deep_morphology.data.seq2seq_data import GlobalPaddingInflectionDataset, UnlabeledGlobalPaddingInflectionDataset
 from deep_morphology.data.reinflection_data import ReinflectionDataset, UnlabeledReinflectionDataset
 from deep_morphology.data.tagging_data import TaggingDataset, UnlabeledTaggingDataset
 from deep_morphology.data.classification_data import ClassificationDataset, UnlabeledClassificationDataset
--- deep_morphology/data/seq2seq_data.py
+++ deep_morphology/data/seq2seq_data.py
@@ -116,3 +116,98 @@ class UnlabeledInflectionDataset(InflectionDataset):
         src = ["<L>"] + list(lemma) + ["</L>", "<T>"] + tags + ["</T>"]
         tgt = None
         return Seq2seqWithLenFields(src, tgt, len(src), None)
+
+
+class GlobalPaddingInflectionDataset(InflectionDataset):
+
+    unlabeled_data_class = 'UnlabeledGlobalPaddingInflectionDataset'
+
+    def to_idx(self):
+        mtx = self.create_recordclass(*[[] for _ in range(len(self.raw[0]))])
+        if hasattr(self.config, 'maxlen_src'):
+            self.maxlen_src = self.config.maxlen_src
+        else:
+            self.maxlen_src = max(len(s.src) for s in self.raw) + 1
+            self.config.maxlen_src = self.maxlen_src
+        self.maxlen_tgt = max(len(s.tgt) for s in self.raw) + 1
+        PAD_src = self.vocabs.src['PAD']
+        PAD_tgt = self.vocabs.tgt['PAD']
+        EOS = self.vocabs.src['EOS']
+
+        for sample in self.raw:
+            mtx.src_len.append(sample.src_len)
+            mtx.tgt_len.append(sample.tgt_len)
+            src = [self.vocabs.src[s] for s in sample.src]
+            src.append(EOS)
+            src.extend([PAD_src] * (self.maxlen_src - len(src)))
+            mtx.src.append(src)
+            tgt = [self.vocabs.tgt[s] for s in sample.tgt]
+            tgt.append(EOS)
+            tgt.extend([PAD_tgt] * (self.maxlen_tgt - len(tgt)))
+            mtx.tgt.append(tgt)
+        self.mtx = mtx
+
+    def batched_iter(self, batch_size):
+        for start in range(0, len(self), batch_size):
+            end = start + batch_size
+            yield self.create_recordclass(*(m[start:end] for m in self.mtx))
+
+
+class UnlabeledGlobalPaddingInflectionDataset(GlobalPaddingInflectionDataset):
+
+    def to_idx(self):
+        mtx = self.create_recordclass(*[[] for _ in range(len(self.raw[0]))])
+        if hasattr(self.config, 'maxlen_src'):
+            self.maxlen_src = self.config.maxlen_src
+        else:
+            self.maxlen_src = max(len(s.src) for s in self.raw)
+            self.config.maxlen_src = self.maxlen_src
+        PAD_src = self.vocabs.src['PAD']
+        EOS = self.vocabs.src['EOS']
+
+        for sample in self.raw:
+            mtx.src_len.append(sample.src_len)
+            mtx.tgt_len.append(None)
+            src = [self.vocabs.src[s] for s in sample.src]
+            src.append(EOS)
+            src.extend([PAD_src] * (self.maxlen_src - len(src)))
+            mtx.src.append(src)
+            mtx.tgt.append(None)
+
+        self.mtx = mtx
+
+    def print_sample(self, sample, stream):
+        lidx = sample.src.index("</L>")
+        lemma = "".join(sample.src[1:lidx])
+        tags = ";".join(sample.src[lidx+2:-1])
+        if len(sample.tgt) > 0:
+            if sample.tgt[0] == "<I>":
+                sample.tgt = sample.tgt[1:]
+        if len(sample.tgt) > 0:
+            if sample.tgt[-1] == "</I>":
+                sample.tgt = sample.tgt[:-1]
+        inflected = "".join(sample.tgt)
+        stream.write("{}\t{}\t{}\n".format(
+            lemma, inflected, tags
+        ))
+
+    def decode(self, model_output):
+        assert len(model_output) == len(self.mtx[0])
+        for i, sample in enumerate(self.raw):
+            output = list(model_output[i])
+            decoded = [self.vocabs.tgt.inv_lookup(s)
+                       for s in output]
+            if decoded[0] == 'SOS':
+                decoded = decoded[1:]
+            if 'EOS' in decoded:
+                decoded = decoded[:decoded.index('EOS')]
+            self.raw[i].tgt = decoded
+
+    def extract_sample_from_line(self, line):
+        fd = line.strip().split("\t")
+        lemma = fd[0]
+        tags = fd[-1]
+        tags = tags.split(";")
+        src = ["<L>"] + list(lemma) + ["</L>", "<T>"] + tags + ["</T>"]
+        tgt = None
+        return Seq2seqWithLenFields(src, tgt, len(src), None)
