fe045fea5652a5861d33e4e11196ecffa342b6f5
Judit Acs <judit@sch.bme.hu>
2018-10-26 19:30:17 +0200

    mask pad symbols

--- deep_morphology/models/attention.py
+++ deep_morphology/models/attention.py
@@ -26,7 +26,7 @@ class LuongAttention(nn.Module):
         self.method = method
         self.softmax = nn.Softmax(dim=-1)
 
-    def forward(self, encoder_outputs, decoder_output):
+    def forward(self, encoder_outputs, decoder_output, encoder_lens):
         if self.method == 'general':
             energy = self.attn_weight(encoder_outputs)
         elif self.method == 'dot':
@@ -38,8 +38,13 @@ class LuongAttention(nn.Module):
             energy = self.attn_weight(concat)
         energy = energy.transpose(0, 1).bmm(decoder_output.permute(1, 2, 0))
         energy = energy.squeeze(2)
+        batch_size, maxlen = energy.size()
+        m = encoder_lens.unsqueeze(1).expand(energy.size())
+        rang = torch.arange(maxlen, dtype=torch.long).unsqueeze(0).expand(energy.size())
+        if torch.cuda.is_available():
+            rang = rang.cuda()
+        mask = m <= rang
+        energy[mask] = float('-inf')
         attention = self.softmax(energy)
         context = attention.unsqueeze(1).bmm(encoder_outputs.transpose(0, 1))
         return context
-
-
