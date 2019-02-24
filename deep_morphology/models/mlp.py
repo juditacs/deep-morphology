572b15f44593f05032a6d3a2bb2dd2d0fe53314e
Judit Acs <judit@sch.bme.hu>
2019-02-24 22:46:23 +0100

    do not use nonlinearity after output layer

--- deep_morphology/models/mlp.py
+++ deep_morphology/models/mlp.py
@@ -14,30 +14,17 @@ class MLP(nn.Module):
         mlp_layers = []
         if not isinstance(nonlinearity, list):
             nonlinearity = [nonlinearity] * (len(layers) + 1)
-        layers = [input_size] + layers + [output_size]
+        layers = [input_size] + layers
         mlp_layers = []
+        # input and hidden layers
         for i in range(0, len(layers)-1):
             n = layers[i]
             m = layers[i+1]
             mlp_layers.append(nn.Linear(n, m))
             mlp_layers.append(getattr(nn, nonlinearity[i])())
+        # output layer without nonlinearity
+        mlp_layers.append(nn.Linear(layers[-1], output_size))
         self.layers = nn.Sequential(*mlp_layers)
-        return
-
-        if not layers:
-            mlp_layers.append(nn.Linear(input_size, output_size))
-        else:
-            # input layer
-            mlp_layers.append(nn.Linear(input_size, layers[0]))
-            mlp_layers.append(getattr(nn, nonlinearity[0])())
-            # hidden layers
-            for i in range(1, len(layers)):
-                mlp_layers.append(nn.Linear(layers[i-1], layers[i]))
-                mlp_layers.append(getattr(nn, nonlinearity[i+1])())
-            # output layer
-            mlp_layers.append(nn.Linear(layers[-1], output_size))
-        self.layers = nn.Sequential(*mlp_layers)
-        print(self.layers)
 
     def forward(self, input):
         return self.layers(input)
