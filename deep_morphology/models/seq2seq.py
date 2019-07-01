6f3c5a08c823bbb0bb8c11c745357a23230a1f28
Judit Acs <judit@sch.bme.hu>
2019-07-01 12:33:41 -0700

    Revert "remove has_target, use nn.Module.training instead"
    This reverts commit 25b6ea380dea81bb9ed0e17e8da5638a6c83afe4.

--- deep_morphology/models/seq2seq.py
+++ deep_morphology/models/seq2seq.py
@@ -244,10 +244,10 @@ class Seq2seq(BaseModel):
                 setattr(self, param_tgt, getattr(self.config, param_tgt))
 
     def forward(self, batch):
-        is_train = self.training
+        has_target = batch.tgt is not None and batch.tgt[0] is not None
         X = to_cuda(torch.LongTensor(batch.src)).transpose(0, 1)  # time major
         seqlen_src, batch_size = X.size()
-        if is_train:
+        if has_target:
             Y = to_cuda(torch.LongTensor(batch.tgt)).transpose(0, 1)
             seqlen_tgt = Y.size(0)
         else:
@@ -256,7 +256,7 @@ class Seq2seq(BaseModel):
         tf_mode = getattr(self.config, 'teacher_forcing_mode', 'always')
         tf_prob = getattr(self.config, 'teacher_forcing_prob', 0.5)
         assert tf_mode in ('always', 'batch', 'sample', 'symbol')
-        if is_train is False:
+        if has_target is False:
             do_tf = False
         elif tf_mode == 'batch':
             do_tf = np.random.random() < tf_prob
