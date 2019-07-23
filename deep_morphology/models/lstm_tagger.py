94a70ef8b9070439b8a1289cf546d59a750911d6
Judit Acs <judit@sch.bme.hu>
2019-07-23 14:11:54 -0700

    replace embedding with EmbeddingWrapper, add option for one hot embedding, use packed lstm

--- deep_morphology/models/lstm_tagger.py
+++ deep_morphology/models/lstm_tagger.py
@@ -10,6 +10,8 @@ import torch
 import torch.nn as nn
 
 from deep_morphology.models import BaseModel
+from deep_morphology.models.packed_lstm import AutoPackedLSTM
+from deep_morphology.models.embedding import EmbeddingWrapper, OneHotEmbedding
 
 
 use_cuda = torch.cuda.is_available()
@@ -26,15 +28,19 @@ class LSTMTagger(BaseModel):
         super().__init__(config)
         input_size = len(dataset.vocabs.src)
         output_size = len(dataset.vocabs.tgt)
-        self.hidden_size = self.config.hidden_size_src
-        self.embedding_dropout = nn.Dropout(self.config.dropout)
-        self.embedding = nn.Embedding(
-            input_size, self.config.embedding_size_src)
-        nn.init.xavier_uniform_(self.embedding.weight)
-        self.cell = nn.LSTM(self.config.embedding_size_src, self.hidden_size,
-                            batch_first=False, dropout=self.config.dropout,
-                            num_layers=self.config.num_layers_src,
-                            bidirectional=True)
+        self.hidden_size = self.config.hidden_size
+        if self.config.use_one_hot_embedding:
+            self.embedding = OneHotEmbedding(input_size)
+            self.embedding_size = input_size
+        else:
+            self.embedding = EmbeddingWrapper(
+                input_size, self.config.embedding_size,
+                dropout=self.config.dropout)
+            self.embedding_size = self.config.embedding_size
+        self.cell = AutoPackedLSTM(self.embedding_size, self.hidden_size // 2,
+                                   batch_first=False, dropout=self.config.dropout,
+                                   num_layers=self.config.num_layers,
+                                   bidirectional=True)
         self.out_proj = nn.Linear(self.hidden_size, output_size)
         self.criterion = nn.CrossEntropyLoss(
             ignore_index=dataset.vocabs.tgt['PAD'])
@@ -43,10 +49,15 @@ class LSTMTagger(BaseModel):
         input = to_cuda(torch.LongTensor(batch.src))
         input = input.transpose(0, 1)  # time_major
         embedded = self.embedding(input)
-        embedded = self.embedding_dropout(embedded)
-        outputs, hidden = self.cell(embedded)
-        outputs = outputs[:, :, :self.hidden_size] + \
-            outputs[:, :, self.hidden_size:]
+        outputs, (h, c) = self.cell(embedded, batch.src_len)
+        num_layers = self.config.num_layers
+        num_directions = 2
+        batch = input.size(1)
+        hidden_size = self.config.hidden_size // 2
+        h = h.view(num_layers, num_directions, batch, hidden_size)
+        c = c.view(num_layers, num_directions, batch, hidden_size)
+        h = torch.cat((h[:, 0], h[:, 1]), 2)
+        c = torch.cat((c[:, 0], c[:, 1]), 2)
         outputs = self.out_proj(outputs)
         return outputs.transpose(0, 1)
 
